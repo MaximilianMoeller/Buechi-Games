554b82a574a541fc1831083497f4d288fe05eb14
Max Moeller <maximilian.moeller@mailbox.tu-dresden.de>
2023-12-29 18:10:26 +0100

    refactor, networkx does not work well with loops and square vertices

--- game.py
+++ game.py
@@ -7,46 +7,93 @@ class Player(Enum):
     Eve = auto()
     Adam = auto()
 
-Eve = Player.Eve
-Adam = Player.Adam
-
 class Color(StrEnum):
     Win = "#0069b4"
     Other = "black"
 
 # (owner, position_x, position_y)
 nodes = [
-        (0, {"owner": Eve, "pos": (0,0)}),
-        (1, {"owner": Eve, "pos": (0,1)}),
-        (2, {"owner": Adam, "pos": (1,0)}),
-        (3, {"owner": Adam, "pos": (1,1)}),
+        (0, {"owner": Player.Eve, "pos": (0,1)}),
+        (1, {"owner": Player.Adam, "pos": (1,1)}),
+        (2, {"owner": Player.Eve, "pos": (2,1)}),
+        (3, {"owner": Player.Eve, "pos": (3,1)}),
+        (4, {"owner": Player.Adam, "pos": (0,0)}),
+        (5, {"owner": Player.Eve, "pos": (1,0)}),
+        (6, {"owner": Player.Adam, "pos": (2,0)}),
+        (7, {"owner": Player.Adam, "pos": (3,0)}),
         ]
 edges = [
-        (0, 1, {"color": Color.Win}),
-        (0, 2, {"color": Color.Other}),
-        (1, 3, {"color": Color.Other}),
+        (0, 1, {"color": Color.Other}),
+        (0, 4, {"color": Color.Other}),
+        (1, 2, {"color": Color.Other}),
+        (1, 4, {"color": Color.Other}),
         (2, 3, {"color": Color.Other}),
+        (2, 6, {"color": Color.Other}),
+        (3, 2, {"color": Color.Other}),
+        (3, 7, {"color": Color.Other}),
+        (4, 0, {"color": Color.Other}),
+        (4, 5, {"color": Color.Other}),
+        (5, 5, {"color": Color.Other}),
+        (5, 6, {"color": Color.Other}),
+        (6, 5, {"color": Color.Other}),
+        (6, 7, {"color": Color.Other}),
+        (7, 7, {"color": Color.Win}),
         ]
 
-game = nx.DiGraph()
-game.add_nodes_from(nodes)
-game.add_edges_from(edges)
+class Game(nx.DiGraph):
 
-eve_nodes = nx.subgraph_view(game, filter_node=lambda n: game.nodes[n]["owner"]==Eve)
-adam_nodes = nx.subgraph_view(game, filter_node=lambda n: game.nodes[n]["owner"]==Adam)
+    @classmethod
+    def construct(cls, nodes, edges, **attr):
+        obj = cls(attr)
+        obj.add_nodes_from(nodes)
+        obj.add_edges_from(edges)
+        return obj
+
+    def eves_nodes(self):
+        return nx.subgraph_view(self, filter_node=lambda n: self.nodes[n]["owner"]==Player.Eve)
+
+    def adams_nodes(self):
+        return nx.subgraph_view(self, filter_node=lambda n: self.nodes[n]["owner"]==Player.Adam)
+
+    def draw_nodes(self):
+        node_options = {"edgecolors": "black", "node_size": 400, "node_color": "white"}
+
+        pos = nx.get_node_attributes(self, "pos")
+        nx.draw_networkx_nodes(self, pos, self.eves_nodes(), node_shape='o', **node_options)
+        nx.draw_networkx_nodes(self, pos, self.adams_nodes(), node_shape='s', **node_options)
+
+        nx.draw_networkx_labels(self, pos)
+
+    def draw_edges(self):
+        pos = nx.get_node_attributes(self, "pos")
+        edge_options = {"arrowsize": 20, "node_size": 400}
+
+        bi_edges = list(filter(lambda e: e[0] != e[1] and (e[1], e[0]) in self.edges, self.edges))
+        bi_edges_colors = list(map(lambda e: self.edges[e]["color"], bi_edges))
+        nx.draw_networkx_edges(self, pos, edgelist=bi_edges, edge_color=bi_edges_colors, connectionstyle='arc3,rad=0.2', **edge_options)
+
+        single_edges = list(filter(lambda e: (e[1], e[0]) not in self.edges, self.edges))
+        single_edges_colors = list(map(lambda e: self.edges[e]["color"], single_edges))
+        nx.draw_networkx_edges(self, pos, edgelist=single_edges, edge_color=single_edges_colors, **edge_options)
+
+        loop_edges = list(filter(lambda e: e[0] == e[1], self.edges))
+        loop_edges_colors = list(map(lambda e: self.edges[e]["color"], loop_edges))
+        nx.draw_networkx_edges(self, pos, edgelist=loop_edges, edge_color=loop_edges_colors, node_size=1000, arrowsize=20)
 
 
-pos = nx.get_node_attributes(game, "pos")
-print(game.edges(data=True))
-edge_colors = [c for (u,v,c) in game.edges.data('color')]
-
-node_options = {"edgecolors": "black", "node_size": 400, "node_color": "white"}
-nx.draw_networkx_nodes(game, pos, eve_nodes, node_shape='o', **node_options)
-nx.draw_networkx_nodes(game, pos, adam_nodes, node_shape='s', **node_options)
-nx.draw_networkx_edges(game, pos, edge_color=edge_colors, )
 
 
 
-plt.axis("off")
+game = Game.construct(nodes, edges)
+
+game.draw_nodes()
+game.draw_edges()
+
+ax = plt.gca()
+ax.set_aspect('equal', 'box')
+ax.set(xlim=(-1, 4), ylim=(-1, 2))
+ax.axis('off')
+
 plt.tight_layout()
+
 plt.show()
